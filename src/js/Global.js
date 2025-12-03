0208609703e0fdd918e0a4f72adc033af837d0ae
Ashish Kumar <info@cxo.studio>
2025-12-03 11:53:02 +0530

    fix header or makeup section

--- src/js/Global.js
+++ src/js/Global.js
@@ -3,18 +3,26 @@ import AOS from "aos";
 
 $(document).ready(function () {
   $(".menu-click").on("click", function () {
-    $(".navigation").css("width", "100%");
+    $(".navigation").fadeIn();
     $("body").addClass("overflow-hidden");
   });
 
   $(".menu-close").on("click", function () {
-    $(".navigation").css("width", "0%");
+   $(".navigation").fadeOut();
     $("body").removeClass("overflow-hidden");
   });
   $(".slider").on("beforeChange", function () {
     $(".slick-slide::after").css({ width: 0, opacity: 0 });
   });
 });
+
+$(window).resize(function() {
+  if(window.innerWidth > 1023) {
+    $("body").removeClass("overflow-hidden");
+    $(".navigation").removeAttr("style")
+  }
+});
+
 AOS.init({
   once: true,
 });
