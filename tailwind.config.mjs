14c60241188b1f4487780d61a8d23d087d02da40
Ashish Kumar <info@cxo.studio>
2025-12-08 13:22:17 +0530

    fix header section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,md,mdx,svelte}"],
+    safelist: [
+    "after:w-full",
+    "after:w-0",
+    "text-gray-300",
+  ],
   theme: {
     extend: {
       fontFamily: {
