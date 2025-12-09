e6f1b5c14a7929b55cb39932522deb380de0fa74
Ashish Kumar <info@cxo.studio>
2025-12-09 11:30:48 +0530

    fix navbar selected

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,md,mdx,svelte}"],
-    safelist: [
-    "after:w-full",
-    "after:w-0",
-    "text-gray-300",
-  ],
   theme: {
     extend: {
       fontFamily: {
