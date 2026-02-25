a1e280f92c41c04acf9cc0c2e1f56576b73e0833
wft-dev <dev@myweb-development.com>
2026-02-25 12:55:40 +0530

    add custom pages sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  site: "https://www.feminaplussalon.com",
+  integrations: [tailwind(), sitemap({
+    customPages: [
+    'https://www.feminaplussalon.com',
+    'https://www.feminaplussalon.com/makeup/',
+    'https://www.feminaplussalon.com/hair-color/',
+    'https://www.feminaplussalon.com/hair-patch-wigs/'
+  ]})],
 });
