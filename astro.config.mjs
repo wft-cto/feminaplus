8f36acfb3b8684385e2dc81cea78ecb831727eb0
wft-dev <dev@myweb-development.com>
2026-02-25 13:19:23 +0530

    remove the home page link in sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ export default defineConfig({
   site: "https://www.feminaplussalon.com",
   integrations: [tailwind(), sitemap({
     customPages: [
-    'https://www.feminaplussalon.com',
     'https://www.feminaplussalon.com/makeup/',
     'https://www.feminaplussalon.com/hair-color/',
     'https://www.feminaplussalon.com/hair-patch-wigs/'
